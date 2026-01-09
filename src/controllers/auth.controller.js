d4b8866c001c82a235221065ba5c9c2b33b6f08a
Gitartha123 <gitarthapuzari@gmail.com>
2026-01-09 13:20:46 +0530

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -11,16 +11,15 @@ exports.generateAuthToken = async (req, res) => {
   if (!name || !email)
     return res.status(400).json({ msg: "Name and email required" });
 
-
   // 1️⃣ Find token record
   const tokenDoc = await Token.findOne({
-    name:name,
-    email:email,
-    machineHash:machineHash,
+    name: name,
+    email: email,
+    machineHash: machineHash,
     active: true
   });
 
- 
+
   if (!tokenDoc)
     return res.status(404).json({ msg: "License not found" });
 
@@ -40,4 +39,51 @@ exports.generateAuthToken = async (req, res) => {
   );
 
   res.json({ token: jwtToken });
+};
+
+exports.registerToken = async (req, res) => {
+    try {
+        const {
+            name,
+            email,
+            productId,
+            machineHash,
+            validTill
+        } = req.body;
+
+        console.log(req.body)
+        // ✅ Validation (THIS WILL NOW WORK)
+        if (
+            !name ||
+            !email ||
+            !productId||
+            !machineHash ||
+            !validTill
+        ) {
+            return res.status(400).json({
+                msg: "All fields are required"
+            });
+        }
+
+        // ✅ Save to DB
+        await Token.create({
+            name,
+            email,
+            productId,
+            machineHash,
+            validTill
+        });
+
+        return res.status(200).json({
+            msg: "Registered Successfully !!"
+        });
+
+        
+
+    } catch (error) {
+        console.error("Register error:", error);
+        return res.status(500).json({
+            msg: "Server error"
+        });
+    }
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,5 @@ const controller = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post("/generate-token", controller.generateAuthToken);
-
+router.post("/register",controller.registerToken)
 module.exports = router;
